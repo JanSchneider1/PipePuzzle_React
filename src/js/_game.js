944477e98837dac74e2436aa439900b8239a125d
JanSchneider1 <schneider.jan@adesso.de>
2019-07-29 11:44:48 +0200

    Added tests for '_game.js'

--- src/js/_game.js
+++ src/js/_game.js
@@ -7,21 +7,115 @@ class Game{
             [new TileData("S", 0), new TileData("T", 0), new TileData("L", 0), new TileData("T", 0), new TileData("X", 0), new TileData("E", 2)],
             [new TileData("-", 0), new TileData("T", 0), new TileData("I", 0), new TileData("T", 0), new TileData("L", 0), new TileData("-", 0)],
         ];
-    }
-    evaluateTilemap(tilemap) {
-        var startTile = this.findStartTile(tilemap);
+        this.startPos = this.getStartPos();
+        this.evaluateTileMap();
     }
 
-    findStartTile(tilemap){
-        for(var i = 0; i < tilemap.length; i++) {
-            for(var j = 0; j < tilemap[i].length; j++) {
-                var tile = tilemap[i][j];
+    evaluateTileMap() {
+        var visited = [];
+        this.evaluateTileAtPos(this.startPos[0], this.startPos[1], visited);
+    }
+
+    evaluateTileAtPos(x, y, visited){
+        const positions = this.getMatchingNeighborsOfTileAtPos(x, y);
+        visited.push([x,y]);
+        this.getTileAtPos(x,y).isLit = true;
+        const positionsToVisit = positions.filter(pos => visited.includes([pos[0][1]]));
+        if (positionsToVisit.length === 0) {return;}
+        positionsToVisit.forEach(pos => this.evaluateTileAtPos(pos[0], pos[1], visited));
+    }
+
+    getMatchingNeighborsOfTileAtPos(x, y){
+        if (!this.isInBoundsOfTileMap(x, y)){
+            throw Error("Trying to get matching neighbor tiles of out of bounds position (" + x + " ," + y);
+        }
+        let list = [];
+        const tile = this.getTileAtPos(x,y);
+        const up = this.getUpPos(x,y);
+        const right = this.getRightPos(x,y);
+        const down = this.getDownPos(x,y);
+        const left = this.getLeftPos(x,y);
+
+        if (up !== null && this.matchTilesInDirection(tile, this.getTileAtPos(up[0], up[1]), 'up')) {
+            list.push(up);
+        }
+        if (right !== null && this.matchTilesInDirection(tile, this.getTileAtPos(right[0], right[1]), 'right')){
+            list.push(right);
+        }
+        if (down !== null && this.matchTilesInDirection(tile, this.getTileAtPos(down[0], down[1]), 'down')){
+            list.push(down);
+        }
+        if (left !== null && this.matchTilesInDirection(tile, this.getTileAtPos(left[0], left[1]), 'left')){
+            list.push(left);
+        }
+        return list;
+    }
+
+    getStartPos(){
+        for(var y = 0; y < this.tileMapData.length; y++) {
+            for(var x = 0; x < this.tileMapData[y].length; x++) {
+                var tile = this.tileMapData[y][x];
                 if (tile.type === 'S') {
-                    return tile;
+                    return [x, y];
                 }
             }
         }
         return null;
+    }
+
+    getUpPos(x, y){
+        if (this.isInBoundsOfTileMap(x, y-1)){
+            return [x, y-1];
+        }
+        return null;
+    }
+
+    getRightPos(x, y){
+        if (this.isInBoundsOfTileMap(x+1, y)){
+            return [x+1, y];
+        }
+        return null;
+    }
+
+    getDownPos(x, y){
+        if (this.isInBoundsOfTileMap(x, y+1)){
+            return [x, y+1];
+        }
+        return null;
+    }
+
+    getLeftPos(x, y){
+        if (this.isInBoundsOfTileMap(x-1, y)){
+            return [x-1, y];
+        }
+        return null;
+    }
+
+    getTileAtPos(x,y){
+        return this.tileMapData[y][x];
+    }
+
+    isInBoundsOfTileMap(x, y){
+        return y >= 0 && y < this.tileMapData.length && x >= 0 && x < this.tileMapData[y].length;
+    }
+
+    matchTilesInDirection(tileA, tileB, direction){
+        if (tileA === null|| tileB === null) {
+            throw Error("Trying match tiles that are null (" + tileA + " ," + tileB);
+        }
+        if (direction === 'up'){
+            return tileA.goesUp() && tileB.goesDown();
+        }
+        else if (direction === 'left'){
+            return tileA.goesLeft() && tileB.goesRight();
+        }
+        else if (direction === 'down'){
+            return tileA.goesDown() && tileB.goesUp();
+        }
+        else if (direction === 'right'){
+            return tileA.goesRight() && tileB.goesLeft();
+        }
+        throw Error(direction + " is not a supported value for 'direction' (only 'up', 'left', 'down', 'right'");
     }
 }
 
---
+++ test/_game.test.js
@@ -0,0 +1,136 @@
+const { Game } = require('../src/js/_game.js');
+const { TileData } = require('../src/js/_tiledata.js');
+
+var game = new Game();
+
+beforeEach(() => {
+    game.tileMapData = [
+        [new TileData("-", 0), new TileData("L", 0), new TileData("L", 0), new TileData("I", 0), new TileData("I", 0), new TileData("-", 0)],
+        [new TileData("S", 0), new TileData("T", 0), new TileData("L", 0), new TileData("T", 0), new TileData("X", 0), new TileData("E", 2)],
+        [new TileData("-", 0), new TileData("T", 0), new TileData("I", 0), new TileData("T", 0), new TileData("L", 0), new TileData("-", 0)],
+    ];
+});
+
+describe('Test if x and y are in bounds of tilemap and return true', () => {
+    test.each`
+    x    | y
+   
+    ${0} | ${0}
+    ${5} | ${2}
+    ${3} | ${2}
+   
+    `(`(x = $x, y = $y) is in bounds of tilemap (xMax = ${game.tileMapData[0].length-1}, yMax = ${game.tileMapData.length-1})`, ({x, y}) => {
+        expect(game.isInBoundsOfTileMap(x,y)).toBe(true);
+    })
+});
+
+describe('Test if x and y are in out of bounds of tilemap and return false', () => {
+    test.each`
+    x    | y
+   
+    ${-1} | ${0}
+    ${0}  | ${-1}
+    ${-1} | ${-1}
+    ${10} | ${0}
+    ${0}  | ${10}
+    ${10} | ${10}
+   
+    `(`(x = $x, y = $y) is out of bounds of tilemap (xMax = ${game.tileMapData[0].length-1}, yMax = ${game.tileMapData.length-1})`, ({x, y}) => {
+        expect(game.isInBoundsOfTileMap(x,y)).toBe(false);
+    })
+});
+
+test("getTileAtPos with x = 5 and y = 1 returns element of array[1][5]", () => {
+    expect(game.getTileAtPos(5,1)).toEqual(game.tileMapData[1][5]);
+});
+
+describe('Test if tiles matches is true', () => {
+    test.each`
+    tileA                   | tileB                   | direction 
+   
+    ${new TileData('I', 1)} | ${new TileData('I', 1)} | ${'up'}
+    ${new TileData('I', 0)} | ${new TileData('I', 0)} | ${'right'}
+    ${new TileData('I', 1)} | ${new TileData('I', 1)} | ${'down'}
+    ${new TileData('I', 0)} | ${new TileData('I', 0)} | ${'left'}
+   
+    `('$tileA matches $tileB in direction $direction', ({tileA, tileB, direction}) => {
+        expect(game.matchTilesInDirection(tileA, tileB, direction)).toBe(true);
+    })
+});
+
+describe('Test if tiles matches is false', () => {
+    test.each`
+    tileA                   | tileB                   | direction 
+   
+    ${new TileData('I', 0)} | ${new TileData('I', 0)} | ${'up'}
+    ${new TileData('I', 1)} | ${new TileData('I', 1)} | ${'right'}
+    ${new TileData('I', 0)} | ${new TileData('I', 0)} | ${'down'}
+    ${new TileData('I', 1)} | ${new TileData('I', 1)} | ${'left'}
+   
+    `('$tileA doesn\'t matches $tileB in direction $direction', ({tileA, tileB, direction}) => {
+        expect(game.matchTilesInDirection(tileA, tileB, direction)).toBe(false);
+    })
+});
+
+test("Invalid direction 'south' throws error when trying to match tiles", () => {
+    expect(() => game.matchTilesInDirection(new TileData('I', 0), new TileData('I', 0), 'south')).toThrowError();
+});
+test("When tileA is null throw error on matching tiles", () => {
+    expect(() => game.matchTilesInDirection(null, new TileData('I', 0), 'up')).toThrowError();
+});
+test("When tileB is null throw error on matching tiles", () => {
+    expect(() => game.matchTilesInDirection(new TileData('I', 0), null, 'up')).toThrowError();
+});
+
+test("getUpPos returns (0,1) when given (0,2)", () => {
+    expect(game.getUpPos(0,2)).toEqual([0,1]);
+});
+test("getUpPos returns null when given (0,0)", () => {
+    expect(game.getUpPos(0,0)).toBe(null);
+});
+
+test("getRightPos returns (1,2) when given (0,2)", () => {
+    expect(game.getRightPos(0,2)).toEqual([1,2]);
+});
+test("getRightPos returns null when given (5,0)", () => {
+    expect(game.getRightPos(5,0)).toBe(null);
+});
+
+test("getLeftPos returns (0,2) when given (1,2)", () => {
+    expect(game.getLeftPos(1,2)).toEqual([0,2]);
+});
+test("getLeftPos returns null when given (0,0)", () => {
+    expect(game.getLeftPos(0,0)).toBe(null);
+});
+
+test("getDownPos returns (0,2) when given (0,1)", () => {
+    expect(game.getDownPos(0,1)).toEqual([0,2]);
+});
+test("getDownPos returns null when given (2,0)", () => {
+    expect(game.getDownPos(0,2)).toBe(null);
+});
+
+test("getStartPos return (0,1)", () => {
+    expect(game.getStartPos()).toEqual([0,1]);
+});
+
+test("getMatchingNeighborsOfTileAtPos throws error when x and y is out of bounds", () => {
+    expect(() => game.getMatchingNeighborsOfTileAtPos(-1, -1)).toThrowError();
+});
+
+describe('Test if getMatchingNeighborsOfTileAtPos return the expected list of tiles', () => {
+    test.each`
+    x    | y    | expectedList 
+
+    ${0} | ${0} | ${[]}
+    ${0} | ${1} | ${[[1, 1]]}
+    ${1} | ${1} | ${[[2, 1],[0, 1]]}
+    ${2} | ${1} | ${[[2, 0],[1, 1]]}
+   
+    `(`The matching neighbor tiles of ($x, $y) are $expectedList`, ({x, y, expectedList}) => {
+        expect(game.getMatchingNeighborsOfTileAtPos(x,y)).toEqual(expectedList);
+    })
+});
+
+
+
