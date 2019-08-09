afcc84a3de397b7c6ae09db090023279d1e29d70
JanSchneider1 <schneider.jan@adesso.de>
2019-08-09 15:56:18 +0200

    Added snapshot test for Tile Component and added 'Enzyme' for Component Testing. Added tests for Tile Component to test if expected img src is chosen based on TileData.

--- test/Tile.react.test.js
+++ test/Tile.react.test.js
@@ -1,5 +1,6 @@
 const React = require('react');
 const { create } = require('react-test-renderer');
+const { shallow } = require('enzyme');
 
 const { Tile } = require("../src/js/components/Tile.react");
 const { TileData } = require("../src/js/_tiledata");
@@ -10,4 +11,30 @@ test('Creating Tile matches Snapshot', () => {
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+});
+
+describe('Test if created tile has the expected type', () => {
+    test.each`
+    
+    type    | isLit    | expectedImgSrc
+    ${'-'}  | ${false} | ${"/img/Empty_Tile.png"}
+    ${'S'}  | ${false} | ${"/img/Start.png"}
+    ${'E'}  | ${false} | ${"/img/End.png"}
+    ${'I'}  | ${false} | ${"/img/I-Tile.png"}
+    ${'L'}  | ${false} | ${"/img/L-Tile.png"}
+    ${'T'}  | ${false} | ${"/img/T-Tile.png"}
+    ${'X'}  | ${false} | ${"/img/X-Tile.png"}
+    ${'-'}  | ${true}  | ${"/img/Empty_Tile.png"}
+    ${'S'}  | ${true}  | ${"/img/Start.png"}
+    ${'E'}  | ${true}  | ${"/img/End_Lit.png"}
+    ${'I'}  | ${true}  | ${"/img/I-Tile_Lit.png"}
+    ${'L'}  | ${true}  | ${"/img/L-Tile_Lit.png"}
+    ${'T'}  | ${true}  | ${"/img/T-Tile_Lit.png"}
+    ${'X'}  | ${true}  | ${"/img/X-Tile_Lit.png"}
+    
+    `('Creating Tile with TileData{$type, 0} and isLit = $isLit renders component with expected img src = $expectedImgSrc', ({ type, isLit, expectedImgSrc }) => {
+        let tileData = new TileData(type, 0);
+        tileData.isLit = isLit;
+        expect(shallow(<Tile data={tileData}/>).find('img').prop('src')).toEqual(expectedImgSrc);
+    })
 });
