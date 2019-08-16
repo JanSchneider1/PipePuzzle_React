59d7d39b95045c654cc11c2c855fce9da94ee2be
JanSchneider1 <schneider.jan@adesso.de>
2019-08-16 14:18:56 +0200

    Balancing update

--- src/js/app.js
+++ src/js/app.js
@@ -7,8 +7,8 @@ ReactDOM.render(<GameUI
 
     // Edit to configure game start settings
     startTime={{min: 1, sec: 30}}
-    addTimeOnComplete={{easy: 15, normal: 30, hard: 45}}
-    normalAtStage={2}
-    hardAtStage={5}
+    addTimeOnComplete={{easy: 15, normal: 25, hard: 35}}
+    normalAtStage={4}
+    hardAtStage={8}
 
 />, document.getElementById('app'));
